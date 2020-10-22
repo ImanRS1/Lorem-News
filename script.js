1328d425d320c20de8c809a705551239ae04b969
Reza Soltani <omid-omega@mail.com>
2020-10-22 16:44:41 +0200

    buttons for slide added, initial design of imgs

--- script.js
+++ script.js
@@ -1,3 +1,6 @@
 var currentDate = (new Date()).toString().split(' ').splice(1,3).join(' ');
 document.getElementById("theDate").innerHTML = currentDate;
 
+function plusSlides(i){
+    console.log("test");
+}
