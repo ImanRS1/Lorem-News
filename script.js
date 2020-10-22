a79211d9cc266945a6f671e179db9c4806ea9584
Reza Soltani <omid-omega@mail.com>
2020-10-22 21:50:16 +0200

    added mesh and img background to slider container

--- script.js
+++ script.js
@@ -4,10 +4,19 @@ document.getElementById("theDate").innerHTML = currentDate;
 
 
 var featuredSlideObjects = document.getElementsByClassName("featured-slide-objects");
+var hiddenSlideObjects = document.getElementById("hidden-slide-list");
+
 console.log(featuredSlideObjects);
 
 function plusSlides(i){
-    console.log("test");
+
+    if(i == 1){
+
+
+    }else{
+
+    }
+    console.log(i);
     for(var i = 0; i < featuredSlideObjects.length; i++){
         console.log(featuredSlideObjects[i]);
     }
