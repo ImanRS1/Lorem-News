bf1b1c0d2275433308418674b8bbfc6e3b8a65a4
Reza Soltani <omid-omega@mail.com>
2020-10-22 17:09:11 +0200

    init scripting for slide buttons

--- script.js
+++ script.js
@@ -1,6 +1,14 @@
 var currentDate = (new Date()).toString().split(' ').splice(1,3).join(' ');
 document.getElementById("theDate").innerHTML = currentDate;
 
+
+
+var featuredSlideObjects = document.getElementsByClassName("featured-slide-objects");
+console.log(featuredSlideObjects);
+
 function plusSlides(i){
     console.log("test");
+    for(var i = 0; i < featuredSlideObjects.length; i++){
+        console.log(featuredSlideObjects[i]);
+    }
 }
