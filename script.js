c99f697e9e9f063025935332d952548a32272eb2
Reza Soltani <omid-omega@mail.com>
2020-10-26 19:26:08 +0100

    code cleanup

--- script.js
+++ script.js
@@ -92,48 +92,8 @@ function plusSlides(i){
                 slideCounter = 0;
             }
 
-
-            
-           
-        
-        
-        
-
-
-
-      /*   if(slideCounter < 4){       
-            slideCounter++;            
-        }else{
-            slideCounter = 0;
-        } */
-
-
     }else{
 
     }
-   /*      switch(slideCounter){
-            case 0:
-                for(var x = 0; x<2; x++){
-                    featuredSlideObjects[x].children[0].src = featuredSlideObjects[x+1].children[0].src
-                }
-                featuredSlideObjects[2].children[0].src = hiddenSlideObjects.children[0].src;
-            case 1:
-
-        }
-
-        for(var x = 0; x < 7; x++){
-            if(x > 1){
-                 featuredSlideObjects[x].children[0].src= hiddenSlideObjects.children[x-2].src;
-            }
-            if(x > 2){
-                hiddenSlideObjects.children[x-3].src = hiddenSlideObjects.children[x-1].src;
-            }else{
-                featuredSlideObjects[x].children[0].src = featuredSlideObjects[x+1].children[0].src;
-            }
-        }
-
-    }else{
-
-    } */
 
 }
