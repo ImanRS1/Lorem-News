0ee6f11e2724b3d65d9db2f6ea65b189e79dcfc0
Reza Soltani <omid-omega@mail.com>
2020-10-30 09:43:31 +0100

    plusslide button func revamped, now functioning

--- script.js
+++ script.js
@@ -5,40 +5,99 @@ document.getElementById("theDate").innerHTML = currentDate;
 
 var hiddenSlideImages = document.getElementsByClassName("hidden-slide-list-imgs");
 var hiddenSlideTexts = document.getElementsByClassName("hidden-slide-list-text");
+var slideContent = document.getElementsByClassName("slide-content");
+
+
+
+
+/* 
+slideContent.children[0].removeAttribute("id");
+slideContent.children[0].setAttribute("id", "featured-pic-left"); */
+
+
 slideInit();
+
 
 function slideInit(){
     for(var i = 0; i<hiddenSlideImages.length; i++){
+        //hiddenSlideImages.item(i).removeAttribute("id", "'pic' + 'i'")
         hiddenSlideImages.item(i).setAttribute("id", "hide-content");
     }
-    hiddenSlideImages.item(0).removeAttribute("id", "hide-content");
-    hiddenSlideTexts.item(0).removeAttribute("id", "hide-content");
-    hiddenSlideImages.item(1).removeAttribute("id", "hide-content");
-    hiddenSlideTexts.item(1).removeAttribute("id", "hide-content");
-    hiddenSlideImages.item(2).removeAttribute("id", "hide-content");
-    hiddenSlideTexts.item(2).removeAttribute("id", "hide-content");
 
     hiddenSlideImages.item(0).setAttribute("id", "featured-pic-left");
-    hiddenSlideTexts.item(0).setAttribute("id", "featured-text-left");
     hiddenSlideImages.item(1).setAttribute("id", "featured-pic-middle");
-    hiddenSlideTexts.item(1).setAttribute("id", "featured-text-middle");
     hiddenSlideImages.item(2).setAttribute("id", "featured-pic-right");
-    hiddenSlideTexts.item(2).setAttribute("id", "featured-text-right");
 }
 
+
+
+
+/* if(currentRightSlideSrc.includes("images/featuredpic3.jpg")){
+    console.log("yes");
+} */
 
 
 var slideCounter = 0;
 
 function plusSlides(i){
 
-    document.getElementById("current-slider-bPic").src="images/featuredpic3.jpg";
+    
+var currentRightSlideSrc = document.getElementById("featured-pic-right").src;
+var currentLeftSlideSrc = document.getElementById("featured-pic-left").src;
+var currentMidSlideSrc = document.getElementById("featured-pic-middle").src;
+
+/* console.log(currentRightSlideSrc.substr(67,1));
+console.log(currentMidSlideSrc.substr(67,1));
+console.log(currentLeftSlideSrc.substr(67,1)); */
+
+    //document.getElementById("current-slider-bPic").src="images/featuredpic3.jpg";
 
     if(i == 1){
+        
+      var thisLeftSlide = currentLeftSlideSrc.substr(67,1) - 1;
+      var thisRightSlide = currentRightSlideSrc.substr(67,1) - 1;
+      var thisMidSlide = currentMidSlideSrc.substr(67,1);
 
-        var test = hiddenSlideImages.namedItem("featuredpic3.jpg");
-        //console.log(hiddenSlideImages);
-        console.log(test)
+      console.log(thisMidSlide);
+      
+
+      //document.getElementById("current-slider-bPic").src = "images/featuredpic" + thisMidSlide + ".jpg";
+
+
+      
+      
+      if(thisLeftSlide == 6){
+          slideInit();
+      }else{
+      hiddenSlideImages.item(thisLeftSlide).setAttribute("id", "hide-content");
+      hiddenSlideImages.item(thisLeftSlide + 1).setAttribute("id", "featured-pic-left");
+      if(thisLeftSlide == 5){
+        hiddenSlideImages.item(thisLeftSlide - 5).setAttribute("id", "featured-pic-middle");
+      }else{
+        hiddenSlideImages.item(thisLeftSlide + 2).setAttribute("id", "featured-pic-middle");
+      }
+      if(thisRightSlide == 6){
+          hiddenSlideImages.item(thisRightSlide - 6).setAttribute("id", "featured-pic-right");
+          
+      }else if(thisRightSlide == 0){
+        hiddenSlideImages.item(thisRightSlide + 1).setAttribute("id", "featured-pic-right");
+      }     
+      else{
+      hiddenSlideImages.item(thisLeftSlide + 3).setAttribute("id", "featured-pic-right");
+      }
+
+
+    }
+
+    document.getElementById("current-slider-bPic").src = document.getElementById("featured-pic-middle").src;
+    //document.getElementById("current-slider-bPic").src = "images/featuredpic" + thisMidSlide + ".jpg";
+
+
+
+
+
+
+
        /*  if(slideCounter < 4){
             hiddenSlideImages.item(slideCounter).removeAttribute("id", "featured-pic-left");
             hiddenSlideImages.item(slideCounter).setAttribute("id", "hide-content");
