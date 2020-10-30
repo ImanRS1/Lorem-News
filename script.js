38378a5ea6d714a7604a924add6fdc7723a2994a
Reza Soltani <omid-omega@mail.com>
2020-10-30 13:53:51 +0100

    minusfunc of slider ongoing

--- script.js
+++ script.js
@@ -46,184 +46,62 @@ var currentRightSlideSrc = document.getElementById("featured-pic-right").src;
 var currentLeftSlideSrc = document.getElementById("featured-pic-left").src;
 var currentMidSlideSrc = document.getElementById("featured-pic-middle").src;
 
-/* console.log(currentRightSlideSrc.substr(67,1));
-console.log(currentMidSlideSrc.substr(67,1));
-console.log(currentLeftSlideSrc.substr(67,1)); */
+var thisLeftSlide = currentLeftSlideSrc.substr(67,1) - 1;
+var thisRightSlide = currentRightSlideSrc.substr(67,1) - 1;
+var thisMidSlide = currentMidSlideSrc.substr(67,1);
 
     //document.getElementById("current-slider-bPic").src="images/featuredpic3.jpg";
 
     if(i == 1){
-        
-      var thisLeftSlide = currentLeftSlideSrc.substr(67,1) - 1;
-      var thisRightSlide = currentRightSlideSrc.substr(67,1) - 1;
-      var thisMidSlide = currentMidSlideSrc.substr(67,1);
-
-      console.log(thisMidSlide);
-      
-
-      //document.getElementById("current-slider-bPic").src = "images/featuredpic" + thisMidSlide + ".jpg";
-
-
-      
-      
+    
       if(thisLeftSlide == 6){
-          slideInit();
-      }else{
-      hiddenSlideImages.item(thisLeftSlide).setAttribute("id", "hide-content");
-      hiddenSlideImages.item(thisLeftSlide + 1).setAttribute("id", "featured-pic-left");
-      if(thisLeftSlide == 5){
-        hiddenSlideImages.item(thisLeftSlide - 5).setAttribute("id", "featured-pic-middle");
-      }else{
-        hiddenSlideImages.item(thisLeftSlide + 2).setAttribute("id", "featured-pic-middle");
-      }
-      if(thisRightSlide == 6){
-          hiddenSlideImages.item(thisRightSlide - 6).setAttribute("id", "featured-pic-right");
+            slideInit();
+        }else{
+            hiddenSlideImages.item(thisLeftSlide).setAttribute("id", "hide-content");
+            hiddenSlideImages.item(thisLeftSlide + 1).setAttribute("id", "featured-pic-left");
+
+            if(thisLeftSlide == 5){
+                hiddenSlideImages.item(thisLeftSlide - 5).setAttribute("id", "featured-pic-middle");
+            }else{
+                hiddenSlideImages.item(thisLeftSlide + 2).setAttribute("id", "featured-pic-middle");
+            }
+            if(thisRightSlide == 6){
+                hiddenSlideImages.item(thisRightSlide - 6).setAttribute("id", "featured-pic-right");
           
-      }else if(thisRightSlide == 0){
-        hiddenSlideImages.item(thisRightSlide + 1).setAttribute("id", "featured-pic-right");
-      }     
-      else{
-      hiddenSlideImages.item(thisLeftSlide + 3).setAttribute("id", "featured-pic-right");
-      }
-
-
+            }else if(thisRightSlide == 0){
+                hiddenSlideImages.item(thisRightSlide + 1).setAttribute("id", "featured-pic-right");
+            }     
+        else{
+            hiddenSlideImages.item(thisLeftSlide + 3).setAttribute("id", "featured-pic-right");
+        }
     }
 
     document.getElementById("current-slider-bPic").src = document.getElementById("featured-pic-middle").src;
-    //document.getElementById("current-slider-bPic").src = "images/featuredpic" + thisMidSlide + ".jpg";
-
-
-
-
-
-
-
-       /*  if(slideCounter < 4){
-            hiddenSlideImages.item(slideCounter).removeAttribute("id", "featured-pic-left");
-            hiddenSlideImages.item(slideCounter).setAttribute("id", "hide-content");
-            //den fjärde bilden slyssas ut till hidden
-
-
-            hiddenSlideImages.item(slideCounter+1).removeAttribute("id", "featured-pic-middle");
-            hiddenSlideImages.item(slideCounter+1).setAttribute("id", "featured-pic-left");
-            //den femte bilden får plats vänster
-
-            hiddenSlideImages.item(slideCounter+2).removeAttribute("id", "featured-pic-right");
-            hiddenSlideImages.item(slideCounter+2).setAttribute("id", "featured-pic-middle");
-            //den sjätte bilden får plats mitten
-
-            hiddenSlideImages.item(slideCounter+3).removeAttribute("id", "hide-content");
-            hiddenSlideImages.item(slideCounter+3).setAttribute("id", "featured-pic-right");
-            //den sjunde bilden slyssas in från hidden
-
-            console.log(slideCounter);
-            slideCounter++;
-
-            }else if(slideCounter==4){
-                hiddenSlideImages.item(slideCounter).removeAttribute("id", "featured-pic-left");
-                hiddenSlideImages.item(slideCounter).setAttribute("id", "hide-content");
-
-                hiddenSlideImages.item(slideCounter+1).removeAttribute("id", "featured-pic-middle");
-                hiddenSlideImages.item(slideCounter+1).setAttribute("id", "featured-pic-left");
-
-                hiddenSlideImages.item(slideCounter+2).removeAttribute("id", "featured-pic-right");
-                hiddenSlideImages.item(slideCounter+2).setAttribute("id", "featured-pic-middle");
-
-                hiddenSlideImages.item(slideCounter-4).removeAttribute("id", "hide-content");
-                hiddenSlideImages.item(slideCounter-4).setAttribute("id", "featured-pic-right");
-
-                slideCounter++;
-
-            }else if(slideCounter==5){
-                hiddenSlideImages.item(slideCounter).removeAttribute("id", "featured-pic-left");
-                hiddenSlideImages.item(slideCounter).setAttribute("id", "hide-content");
-
-                hiddenSlideImages.item(slideCounter+1).removeAttribute("id", "featured-pic-middle");
-                hiddenSlideImages.item(slideCounter+1).setAttribute("id", "featured-pic-left");
-
-                hiddenSlideImages.item(slideCounter-5).removeAttribute("id", "featured-pic-right");
-                hiddenSlideImages.item(slideCounter-5).setAttribute("id", "featured-pic-middle");
-
-                hiddenSlideImages.item(slideCounter-4).removeAttribute("id", "hide-content");
-                hiddenSlideImages.item(slideCounter-4).setAttribute("id", "featured-pic-right");
-
-                slideCounter++;
-
-            }else if(slideCounter==6){
-                slideInit();
-                slideCounter = 0;
-            } */
 
     }else{
-        if(slideCounter < 1){
-            /* hiddenSlideImages.item(slideCounter).removeAttribute("id", "featured-pic-left");
-            hiddenSlideImages.item(slideCounter).setAttribute("id", "featured-pic-middle");
+        console.log(thisRightSlide);
 
-
-            hiddenSlideImages.item(slideCounter+1).removeAttribute("id", "featured-pic-middle");
-            hiddenSlideImages.item(slideCounter+1).setAttribute("id", "featured-pic-right");
-
-            hiddenSlideImages.item(slideCounter+2).removeAttribute("id", "featured-pic-right");
-            hiddenSlideImages.item(slideCounter+2).setAttribute("id", "hide-content");
-
-            hiddenSlideImages.item(slideCounter+6).removeAttribute("id", "hide-content");
-            hiddenSlideImages.item(slideCounter+6).setAttribute("id", "featured-pic-left");
-
-            console.log(slideCounter);
-            slideCounter++;
-            }
-        else if(slideCounter == 1){
-            hiddenSlideImages.item(slideCounter).removeAttribute("id", "featured-pic-right");
-            hiddenSlideImages.item(slideCounter).setAttribute("id", "hide-content");
-
-            hiddenSlideImages.item(slideCounter-1).removeAttribute("id", "featured-pic-middle");
-            hiddenSlideImages.item(slideCounter-1).setAttribute("id", "featured-pic-right");
-
-            hiddenSlideImages.item(slideCounter+5).removeAttribute("id", "featured-pic-left");
-            hiddenSlideImages.item(slideCounter+5).setAttribute("id", "featured-pic-middle");
-
-            hiddenSlideImages.item(slideCounter+4).removeAttribute("id", "hide-content");
-            hiddenSlideImages.item(slideCounter+4).setAttribute("id", "featured-pic-left");
-            slideCounter++;
+        hiddenSlideImages.item(thisRightSlide).setAttribute("id", "hide-content");
+        if(thisRightSlide==0){
+            hiddenSlideImages.item(thisRightSlide + 6).setAttribute("id", "featured-pic-right");
+        }else{
+            hiddenSlideImages.item(thisRightSlide - 1).setAttribute("id", "featured-pic-right");
         }
-
-        else if(slideCounter > 1){
-            hiddenSlideImages.item(slideCounter-2).removeAttribute("id", "featured-pic-right");
-            hiddenSlideImages.item(slideCounter-2).setAttribute("id", "hide-content");
-
-            hiddenSlideImages.item(slideCounter+4).removeAttribute("id", "featured-pic-middle");
-            hiddenSlideImages.item(slideCounter+4).setAttribute("id", "featured-pic-right");
-
-            hiddenSlideImages.item(slideCounter+3).removeAttribute("id", "featured-pic-left");
-            hiddenSlideImages.item(slideCounter+3).setAttribute("id", "featured-pic-middle");
-
-            hiddenSlideImages.item(slideCounter+2).removeAttribute("id", "hide-content");
-            hiddenSlideImages.item(slideCounter+2).setAttribute("id", "featured-pic-left");
-            slideCounter++;
-            console.log(slideCounter);
+        if(thisRightSlide == 1){
+            hiddenSlideImages.item(thisRightSlide + 5).setAttribute("id", "featured-pic-middle");
+            hiddenSlideImages.item(thisRightSlide + 4).setAttribute("id", "featured-pic-left");
         }
-
-        else if(slideCounter > 2){
-            hiddenSlideImages.item(slideCounter-2).removeAttribute("id", "featured-pic-right");
-            hiddenSlideImages.item(slideCounter-2).setAttribute("id", "hide-content");
-
-            hiddenSlideImages.item(slideCounter+4).removeAttribute("id", "featured-pic-middle");
-            hiddenSlideImages.item(slideCounter+4).setAttribute("id", "featured-pic-right");
-
-            hiddenSlideImages.item(slideCounter+3).removeAttribute("id", "featured-pic-left");
-            hiddenSlideImages.item(slideCounter+3).setAttribute("id", "featured-pic-middle");
-
-            hiddenSlideImages.item(slideCounter+2).removeAttribute("id", "hide-content");
-            hiddenSlideImages.item(slideCounter+2).setAttribute("id", "featured-pic-left");
-            slideCounter++;
-            console.log(slideCounter);
-
-        } */
-
-/* 
-    } */
-
+        else if(thisRightSlide == 0){
+            hiddenSlideImages.item(thisRightSlide + 5).setAttribute("id", "featured-pic-middle");
+            hiddenSlideImages.item(thisRightSlide + 4).setAttribute("id", "featured-pic-left");
+        }else if(thisRightSlide == 6){
+            hiddenSlideImages.item(thisRightSlide - 2).setAttribute("id", "featured-pic-middle"); 
+            hiddenSlideImages.item(thisRightSlide - 3).setAttribute("id", "featured-pic-left");
+        }        
+        else{
+        hiddenSlideImages.item(thisRightSlide - 2).setAttribute("id", "featured-pic-middle");
+        hiddenSlideImages.item(thisRightSlide + 4).setAttribute("id", "featured-pic-left");
+        }
+    
 }
-
-    }
 }
